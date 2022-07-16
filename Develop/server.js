8ed3b824915552b424406ed1ac76db98a8f55ccc
lundbmp <m.lundberg008@gmail.com>
2022-07-16 12:22:55 -0500

    working post request

--- Develop/server.js
+++ Develop/server.js
@@ -10,6 +10,21 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
+// post and save to db.json
+app.post('/api/notes', (req, res) => {
+    req.body.id = notes.length.toString();
+    const note = req.body;
+    notes.push(note);
+
+    fs.writeFileSync(path.join(__dirname, './db/db.json'), JSON.stringify(notes), error => {
+        if (error) {
+            console.log(error);
+        }
+    });
+
+    res.json(note);
+});
+
 //  returns notes from db.json
 app.get('/api/notes', (req, res) => {
     const results = notes;
